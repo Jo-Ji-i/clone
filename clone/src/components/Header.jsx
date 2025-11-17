5c4bdaef736970028d1a54ba9f011cd047ae6b77
jimmiS2 <gyfl23242@naver.com>
2025-11-17 18:50:33 +0900

    design: 헤더 디자인

--- clone/src/components/Header.jsx
+++ clone/src/components/Header.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
 
 export default function Header() {
   return (
-    <header className="w-full py-4 mb-6 border-b">
-      <nav className="flex gap-4">
-        <Link to="/">Home</Link>
+    <header className="flex items-center justify-between w-full h-32 border-b border-gray-300">
+      <Link to="#">
+        <img src={logo} alt="home" className="w-56 ml-10" />
+      </Link>
+      <nav className="flex justify-around text-lg font-semibold gap-28">
         <Link to="/insights">Insights</Link>
         <Link to="/trends">Quarterly Trends</Link>
         <Link to="/index">Index</Link>
         <Link to="/about">About</Link>
       </nav>
+      <div className="ml-24 mr-16 text-lg font-semibold"> EN </div>
     </header>
   );
 }
