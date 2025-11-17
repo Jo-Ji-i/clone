d5d9e1cab82753e2ff847709f2119dcc097c193f
jimmiS2 <gyfl23242@naver.com>
2025-11-17 17:58:43 +0900

    feat: 라우팅/ 레이아웃 설정

---
+++ clone/src/pages/Index.jsx
@@ -0,0 +1,3 @@
+export default function Index() {
+  return <div className="text-xl font-bold">Index List</div>;
+}
---
+++ clone/src/pages/QuarterlyTrends.jsx
@@ -0,0 +1,3 @@
+export default function QuarterlyTrends() {
+  return <div className="text-xl font-bold">Quarter </div>;
+}
