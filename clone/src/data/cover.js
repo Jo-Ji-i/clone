0af3964bf42a76b6587d6eebc6cb71d3a919ad5b
jimmiS2 <gyfl23242@naver.com>
2025-11-19 12:29:00 +0900

    feat: 데이터 생성

---
+++ clone/src/data/cover.js
@@ -0,0 +1,54 @@
+const InsightCover = [
+  {
+    image: '../assets/insight/img1.png',
+    title:
+      'Vol. 32: Motivations for Overseas Travel and Strategies for Revitalizing Domestic Tourism in Korea',
+    date: '2025.10.22',
+  },
+  {
+    image: '../assets/insight/img2.jpg',
+    title:
+      'Vol.31 Beyond the Ballpark: Exploring the Role of Baseball in Regional Tourism Reg Date',
+    date: '2025.09.26',
+  },
+  {
+    image: '../assets/insight/img3.jpg',
+    title:
+      'Vol.30 Yanolja Attractiveness Index: Measuring the Attractiveness of Global Tourism Cities',
+    date: '2025.08.28',
+  },
+  {
+    image: '../assets/insight/img4.png',
+    title: 'Vol.9 Korea’s Inbound and Outbound Tourism Performance in the First Half of 2025',
+    date: '2025.08.27',
+  },
+];
+
+export const ResearchCover = [
+  {
+    image: '../assets/report/img1.jpg',
+    date: '2025.02.27',
+  },
+  {
+    image: '../assets/report/img2.png',
+    date: '2025.02.27',
+  },
+  {
+    image: '../assets/report/img3.png',
+    date: '2025.02.27',
+  },
+  {
+    image: '../assets/report/img4.png',
+    date: '2025.02.27',
+  },
+  {
+    image: '../assets/report/img5.png',
+    date: '2025.02.27',
+  },
+];
+
+export const NavCover = [
+  { title: 'Yanolja Attractiveness Index', iamge: '../assets/etc/bg_chart.svg' },
+  { title: 'Yanolja Brand Equity Index', image: '../assets/etc/bg_glass.svg' },
+  { title: 'Data Lab', image: '../assets/etc/datalab.svg' },
+];
